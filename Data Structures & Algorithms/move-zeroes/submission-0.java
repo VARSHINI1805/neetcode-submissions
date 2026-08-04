d0cff8c5175ac01b4bdbd49e557aeb512ef64de0
Varshini S <155649877+VARSHINI1805@users.noreply.github.com>
2026-08-04 13:56:58 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int j=0 ;
+        for(int i = 0 ;i  < arr.length ; i++){
+            if(arr[i] != 0){
+                arr[j++] = arr[i];
+            }
+        }
+        for(int i = j ; i < arr.length ; i++ ){
+            arr[i] = 0;
+        }
+    }
+}
